999b826420a7e2988e9ae2f6ef81a84b12dea826
sollendy <edlannino@tutanota.com>
2023-04-20 19:25:58 +0200

    esercizio non funziona

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ createApp({
   data() {
     return {
         // prova: 'ah',
+        punti: [],
+        nuovoPunto: '',
     }
   },
 
@@ -11,7 +13,28 @@ createApp({
     pescaPunti() {
         axios.get('./server.php').then(response => {
             console.log(response.data);
+            this.punti = response.data;
         });
+    },
+
+    mettiPunto() {
+        this.nuovoPunto = '';
+        let dati = {
+            nuovoPunto: '',
+        }
+        dati.nuovoPunto = this.nuovoPunto;
+        //non su usa il push perché non si salverebbe poi nel json
+        //per far sì che il tutto si salvi anche nel json faremo la seguente operazione
+        axios.post('./server.php', data, {headers: {'Content-Type': 'multipart/form-data'}}).then(response => {
+            console.log("ciao grande, la tua chiamata è avvenuta");
+    
+            // posso ricaricare i todo
+            this.pescaPunti();
+          });
     }
-  }
+  },
+
+  mounted() {
+    this.pescaPunti();
+  },
 }).mount('#app')
--- server.php
+++ server.php
@@ -2,10 +2,15 @@
 // adesso vedo se è giusto il modo di verificare la chiamata tramite post e se va fatta al v-model spero di sì altrimenti non so cosa pensare
 if(isset($_POST['nuovoPunto'])) {
     //speriamo che così vada bene adesso dobbiamo lavorare sul json prendendolo innanzitutto
+    $listaJSON = file_get_contents('agenda.json');
+    $puntiLista = json_decode($listaJSON);
+    $puntilista[] = $_POST['nuovoPunto'];
+    $nuovoPuntoJSON = json_encode($puntiLista);
+    file_put_contents('agenda.json', $nuovoPuntoJSON);
+    var_dump($puntiLista);
+} else {
+    $nomeFile = file_get_contents('agenda.json');
+    $puntiLista = json_decode($nomeFile);
+    header('Content-type: application/json');
+    echo json_encode($puntiLista);
 }
-$listaJSON = file_get_contents('agenda.json');
-$puntiLista = json_decode($listaJSON);
-$puntilista[] = $_POST['nuovoPunto'];
-$nuovoPuntoJSON = json_encode($puntiLista);
-file_put_contents('agenda.json', $nuovoPuntoJSON);
-var_dump($puntiLista) ;
